8fca42b459b7e510cbe3073fdb8249ad7174d520
Chakrit Sukmaha <c.sukmaha@gmail.com>
2021-01-18 18:03:03 +0700

    Update index.blade.php
    2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -69,7 +69,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    MAkira
+                    MAkira2
                 </div>
 
                 
